a0c596b2b672cf78b5ca7a604ea52407fe7dcd95
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-09 19:57:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,22 @@
 require 'pry'
 class Application
 
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   t = Time.now
+  #     if t.hour <=12
+  #       resp.write "Good Morning!"
+  #     else
+  #       resp.write "Good Afternoon!"
+  #     end
+  # 
+  #   resp.finish
+  # end
   def call(env)
-    resp = Rack::Response.new
-    t = Time.now
-      if t.hour <=12
-        resp.write "Good Morning!"
-      else
-        resp.write "Good Afternoon!"
-      end
-
-    resp.finish
-  end
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+      resp.finish
+    end
+   
 
 end
